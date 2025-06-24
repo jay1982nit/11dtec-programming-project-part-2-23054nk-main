039f8bfa49799e226923b9f8c12cfbb9f084a869
jay1982nit <jayuapu1982@gmail.com>
2025-06-24 07:00:37 +0000

    Jun 24, 2025, 7:00 AM

--- Rental.js
+++ Rental.js
@@ -112,7 +112,7 @@ function SubmitForm(event)
       document.getElementById("rentcontainer").style.display = "none";
       let WelMsg = document.getElementById("WelMsg")
       document.getElementById("WelMsg").style.display = "block";
-      WelMsg.innerHTML="Kia Ora " + firstName1 + " Let us help you select your dream jewellery. Select from the categories below  (Nityaa to update)";
+      WelMsg.innerHTML="Kia Ora " + firstName1 + " Let us help you select your dream jewellery. Select from the categories below";
       showItems("category");   
   }
   return false; // Ensure form does not reload page
